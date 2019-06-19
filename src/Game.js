4fce26d47c49a40995bed1b2cd591af8675f9039
nrmisturakJPMC <50381272+nrmisturakJPMC@users.noreply.github.com>
2019-06-19 16:14:43 -0400

    Add gameboard

---
+++ src/Game.js
@@ -0,0 +1,3 @@
+import React from "react";
+
+export const Game = () => <div>Hello World</div>;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
+import { Game } from "./Game";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+  return <Game />;
 }
 
 const rootElement = document.getElementById("root");
